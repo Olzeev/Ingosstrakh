ed10a25fe2e6b4fd3b4420d165598449c0b2ef10
Maxim <olzeevmax@gmail.com>
2023-06-10 01:26:25 +0300

    Personal area made, added favicon

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('sign_up', views.sign_up, name='sign_up'),
     path('sign_in', views.sign_in, name='sign_in'), 
-    path('personal_area', views.personal_area, name='area')
+    path('personal_area', views.personal_area_main, name='area1'), 
+    path('edit_info', views.personal_area_edit_info, name='area2'), 
+    path('send_report', views.personal_area_send_report, name='area3'), 
+    path('log', views.personal_area_log, name='area4'), 
+    path('check_sign_up', views.check_sign_up)
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
     return render(request, 'main/index.html', {"is_main": True})
 
@@ -9,9 +10,24 @@ def index(request):
 def sign_up(request):
     return render(request, 'main/sign_up.html', {"is_main": False})
 
-
 def sign_in(request):
     return render(request, 'main/sign_in.html', {"is_main": False})
 
-def personal_area(request):
-    return render(request, 'main/personal_area.html', {'is_main': False})
+def personal_area_main(request):
+    return render(request, 'main/personal_area.html', {'is_main': False,
+                                                       'chosen': 1})
+
+def personal_area_edit_info(request):
+    return render(request, 'main/personal_area_edit_info.html', {'is_main': False, 
+                                                                 'chosen': 2})
+
+def personal_area_send_report(request):
+    return render(request, 'main/personal_area_send_report.html', {'is_main': False, 
+                                                                 'chosen': 3})
+
+def personal_area_log(request):
+    return render(request, 'main/personal_area_log.html', {'is_main': False, 
+                                                           'chosen': 4})
+
+def check_sign_up(request):
+    pass
